e8aeaf0c8171aee90731c0d8724679b3a103390a
Ghifani <ghifani03@gmail.com>
2025-01-16 05:59:53 +0700

    ♻️ Reusable
    Reusable Container

--- frontend/src/components/layout/container.jsx
+++ frontend/src/components/layout/container.jsx
@@ -2,8 +2,8 @@ export default function Container({ children }) {
     return (
         <div className='overflow-y-scroll overflow-x-hidden'>
             <div className='flex justify-center items-center bg-black w-full min-h-screen' >
-            {children}
-        </div>
+                {children}
+            </div>
         </div>
     )
 }
--- frontend/src/routes/RouterMain.jsx
+++ frontend/src/routes/RouterMain.jsx
@@ -1,12 +1,10 @@
 import Login from "../pages/auth/Login"
-import Register from "../pages/auth/Register"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 
 export default function RouterMain() {
     return (
         <Router>
             <Routes>
-                <Route path="/admin/register" element={<Register/>} />
                 <Route path="/admin/login" element={<Login/>} />
             </Routes>
         </Router>
