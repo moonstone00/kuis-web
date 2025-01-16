c637864d5a64ca8decd9b2837211f24c2eb7410c
Ghifani <ghifani03@gmail.com>
2025-01-16 19:34:27 +0700

    🎨 Improve Code
    Improve Code

--- frontend/src/components/layout/container.jsx
+++ frontend/src/components/layout/container.jsx
@@ -1,6 +1,6 @@
 export default function Container({ children }) {
     return (
-        <div className='fle justify-center items-center bg-back py-5 w-fll min-h-screen' >
+        <div className='fle justify-center items-center py-5' >
             <div className='flex flex-col bg-container-form w-[600px] h-min p-6 ' >
                 <div className='flex gap-3'>
                     <div className='bg-red-600 w-4 h-4 rounded-full' ></div>
--- frontend/src/pages/auth/Login.jsx
+++ frontend/src/pages/auth/Login.jsx
@@ -42,62 +42,67 @@ export default function Login() {
     }
 
     useEffect(() => {
+        if(localStorage.getItem('nama') && localStorage.getItem('nip')) {
+            navigate('/admin/dashboard')
+        }
         setTimeout(() => {
             setTime(true)
         }, 800)
     }, [])
 
     return (
-        <Container>
-            <div className='ml-2 my-6 text-[#667694] running-text text-lg'>
-                <p>Creating a new account</p>
-            </div>
+        <section className='bg-black flex items-center justify-center w-full min-h-screen border' >
+            <Container>
+                    <div className='ml-2 my-6 text-[#667694] running-text text-lg'>
+                        <p>Creating a new account</p>
+                    </div>
 
-            <form className='ml-2 mt-4' onSubmit={handleSubmit}>
-                {
-                    time ?
-                    (
+                    <form className='ml-2 mt-4' onSubmit={handleSubmit}>
+                        {
+                            time ?
+                            (
+                                <div className='my-5' >
+                                    <label className='text-cyan-300' >Enter Your NIP*</label>
+                                    <div className='flex items-center mt-2' >
+                                        {
+                                            icon ?
+                                            (
+                                                <img src={done} className='w-4 mr-3' />
+                                            )
+                                            :
+                                            (
+                                                <img src={arrow} className='w-8 mr-3' />
+
+                                            )
+                                        }
+                                        <input type='text' value={NIP} onChange={(event) => handleNIP(event.target.value)} className='text-white focus:outline-none focus:ring-1 focus:ring-[#0969da] bg-input-transparent w-full h-7 rounded-md py-3 px-2 mr-10' required/>
+                                    </div>
+                                </div>
+                            )
+                            :
+                            null
+                        }
+                        
                         <div className='my-5' >
-                            <label className='text-cyan-300' >Enter Your NIP*</label>
+                            <label className='text-cyan-300' >Enter Your Password*</label>
                             <div className='flex items-center mt-2' >
-                                {
-                                    icon ?
-                                    (
-                                        <img src={done} className='w-4 mr-3' />
-                                    )
-                                    :
-                                    (
-                                        <img src={arrow} className='w-8 mr-3' />
-
-                                    )
-                                }
-                                <input type='text' value={NIP} onChange={(event) => handleNIP(event.target.value)} className='text-white focus:outline-none focus:ring-1 focus:ring-[#0969da] bg-input-transparent w-full h-7 rounded-md py-3 px-2 mr-10' required/>
+                                <img src={arrow} className='w-8 mr-3' />
+                                <input type='password'
+                                    style={{
+                                        '-ms-reveal': 'none',
+                                        '-webkit-reveal': 'none',
+                                        'appearance': 'none',
+                                        '-webkit-appearance': 'none' }}
+                                    value={password}
+                                    onChange={(event) => handlePassword(event.target.value)}
+                                    className='text-white focus:outline-none focus:ring-1 focus:ring-[#0969da] bg-input-transparent w-full h-7 rounded-md py-3 px-2 mr-10'
+                                    required
+                                />
+                                <button type='submit' className='w-24 border border-[#a928e0] font-thin px-2 py-1 rounded-md' >Submit</button>
                             </div>
                         </div>
-                    )
-                    :
-                    null
-                }
-                
-                <div className='my-5' >
-                    <label className='text-cyan-300' >Enter Your Password*</label>
-                    <div className='flex items-center mt-2' >
-                        <img src={arrow} className='w-8 mr-3' />
-                        <input type='password'
-                            style={{
-                                '-ms-reveal': 'none',
-                                '-webkit-reveal': 'none',
-                                'appearance': 'none',
-                                '-webkit-appearance': 'none' }}
-                            value={password}
-                            onChange={(event) => handlePassword(event.target.value)}
-                            className='text-white focus:outline-none focus:ring-1 focus:ring-[#0969da] bg-input-transparent w-full h-7 rounded-md py-3 px-2 mr-10'
-                            required
-                        />
-                        <button type='submit' className='w-24 border border-[#a928e0] font-thin px-2 py-1 rounded-md' >Submit</button>
-                    </div>
-                </div>
-            </form>
-        </Container>
+                    </form>
+            </Container>
+        </section>
     )
 }
