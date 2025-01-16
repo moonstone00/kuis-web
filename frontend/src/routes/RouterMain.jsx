20199bb90edada1bd7b53adc9d3964ecd77a043b
Dio putra <dio50btl@gmail.com>
2025-01-16 20:35:16 +0700

    Pages Question

--- frontend/src/routes/RouterMain.jsx
+++ frontend/src/routes/RouterMain.jsx
@@ -1,4 +1,6 @@
 import Login from "../pages/auth/Login"
+import Question from "../pages/auth/question"
+import Questionnaire from "../pages/auth/question"
 import Register from "../pages/auth/Register"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 
@@ -8,6 +10,7 @@ export default function RouterMain() {
             <Routes>
                 <Route path="/admin/register" element={<Register/>} />
                 <Route path="/admin/login" element={<Login/>} />
+                <Route path="/admin/question" element={<Question/>} />
             </Routes>
         </Router>
     )
