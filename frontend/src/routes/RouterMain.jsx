fc275ed5edab0226e7dbc9913ec5d04a24383779
Zakoneeeee <valenmiskah@gmail.com>
2025-01-16 20:17:07 +0700

    Create page not found

--- frontend/src/routes/RouterMain.jsx
+++ frontend/src/routes/RouterMain.jsx
@@ -1,13 +1,13 @@
+import NotFoundPage from "../notfound/notfound"
 import Login from "../pages/auth/Login"
-import Register from "../pages/auth/Register"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 
 export default function RouterMain() {
     return (
         <Router>
             <Routes>
-                <Route path="/admin/register" element={<Register/>} />
-                <Route path="/admin/login" element={<Login/>} />
+                <Route path="/admin/login" element={<Login/>} /> 
+                <Route path="*" element={<NotFoundPage/>} />
             </Routes>
         </Router>
     )
