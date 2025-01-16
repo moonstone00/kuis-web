180a0f97f5f4a48dfbba9fe2934701d45def60ba
Ghifani <ghifani03@gmail.com>
2025-01-16 11:18:28 +0700

    👽️ API Update
    API Update User

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -34,6 +34,30 @@ router.post('/', async (req, res) => {
     }
 })
 
+router.put('/', async (req, res) => {
+    const {nip, nama, password, newPassword} = req.body
+
+    const check = await passwordCheck(nip, password)
+
+    const encryptedPassword = await bcrypt.hash(newPassword, 10)
+
+    if(check.compare) {
+        const users = await UsersModel.update({
+            nama, password: encryptedPassword
+        }, {where: {nip: {nip}}})
+
+        res.status(200).json({
+            users: {update: users[0]},
+            metadata: "User Updated!!!"
+        })
+    } else {
+        res.status(401).json({
+            error: "Data invalid",
+            metadata: password
+        })
+    }
+})
+
 router.post('/login', async (req, res) => {
     const {nip, password} = req.body
     try {
