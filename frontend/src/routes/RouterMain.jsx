f0c0ef7f584063f7f470e95bfcb51b4b44b02801
Ghifani <ghifani03@gmail.com>
2025-01-17 10:33:40 +0700

    ⏪️ Change directory
    Change directory question

--- frontend/src/routes/RouterMain.jsx
+++ frontend/src/routes/RouterMain.jsx
@@ -1,16 +1,16 @@
 import Login from "../pages/auth/Login"
 import NotFoundPage from "../components/notfound/notfound"
 import Dashboard from "../pages/dashboard/Dashboard"
-import Question from "../pages/auth/question"
+import Question from "../pages/question/Question"
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
 
 export default function RouterMain() {
     return (
         <Router>
             <Routes>
+                <Route path="/" element={<Question/>} />
                 <Route path="/admin/login" element={<Login/>} />
                 <Route path="/admin/dashboard" element={<Dashboard/>} />
-                <Route path="/admin/question" element={<Question/>} />
                 <Route path="*" element={<NotFoundPage/>} />
             </Routes>
         </Router>
